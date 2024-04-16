2b570bfc7b524c5cb5fe83a28b7010e67f34fed1
Marcio Borges <marcio.borges@compasso.com.br>
2024-04-16 17:59:27 -0300

    SignIn Screen

--- app/src/main/java/com/creditcard/ui/screens/authenticator/AuthenticatorScreen.kt
+++ app/src/main/java/com/creditcard/ui/screens/authenticator/AuthenticatorScreen.kt
@@ -1,7 +1,6 @@
 package com.creditcard.ui.screens.authenticator
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
--- app/src/main/java/com/creditcard/ui/screens/authenticator/signin/SignInScreen.kt
+++ app/src/main/java/com/creditcard/ui/screens/authenticator/signin/SignInScreen.kt
@@ -1,7 +1,8 @@
 package com.creditcard.ui.screens.authenticator.signin
 
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -10,12 +11,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -25,7 +25,6 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
@@ -35,11 +34,11 @@ import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import com.creditcard.R
 import com.creditcard.ui.theme.Blue40
 import com.creditcard.ui.theme.JetPackComposeCreditCardTheme
-import com.creditcard.ui.theme.Yellow40
 
 @Composable
 fun SignInScreen() {
@@ -51,14 +50,39 @@ fun SignInScreen() {
         modifier = Modifier
             .fillMaxSize()
             .padding(horizontal = 16.dp, vertical = 16.dp),
-        verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Row(modifier = Modifier.padding(0.dp, 32.dp)){
+            Icon(
+                imageVector = ImageVector.vectorResource(id = R.drawable.ic_lock),
+                contentDescription = stringResource(id = R.string.label_email),
+                modifier = Modifier.padding(0.dp, 2.dp, 4.dp, 0.dp)
+            )
+            Text(
+                fontSize = 28.sp,
+                text = stringResource(R.string.title_signin).uppercase(),
+                fontWeight = FontWeight.SemiBold,
+                modifier = Modifier.align(Alignment.Bottom),
+            )
+        }
+
         Text(
-            fontSize = 22.sp,
-            text = stringResource(R.string.title_signin).uppercase(),
-            fontWeight = FontWeight.SemiBold
+            fontSize = 20.sp,
+            text = stringResource(R.string.title_wellcome),
+            fontWeight = FontWeight.SemiBold,
+            modifier = Modifier.align(Alignment.Start)
         )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            fontSize = 14.sp,
+            text = stringResource(R.string.title_wellcome_description),
+            fontWeight = FontWeight.SemiBold,
+            modifier = Modifier.align(Alignment.Start),
+            lineHeight = 1.5.em
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
         OutlinedTextField(
             modifier = Modifier.fillMaxWidth(),
             label = { Text(text = stringResource(id = R.string.label_email)) },
@@ -71,7 +95,9 @@ fun SignInScreen() {
             value = email,
             onValueChange = {}
         )
+
         Spacer(modifier = Modifier.height(16.dp))
+
         OutlinedTextField(
             modifier = Modifier.fillMaxWidth(),
             label = { Text(text = stringResource(id = R.string.label_password)) },
@@ -101,7 +127,20 @@ fun SignInScreen() {
                 }
             }
         )
+
         Spacer(modifier = Modifier.height(16.dp))
+
+        Row(modifier = Modifier.fillMaxWidth())
+        {
+            Switch(checked = true, onCheckedChange = {})
+            Text(
+                text = stringResource(id = R.string.title_keep_connected),
+                modifier = Modifier.align(Alignment.CenterVertically).padding(8.dp, 0.dp)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
         Button(
             modifier = Modifier
                 .fillMaxWidth()
@@ -111,7 +150,23 @@ fun SignInScreen() {
             shape = RoundedCornerShape(10.dp),
             onClick = {},
             content = {
-                Text(text = stringResource(R.string.title_signin).uppercase())
+                Text(text = stringResource(R.string.action_access).uppercase())
+            }
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedButton(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(55.dp),
+
+            enabled = true,
+            shape = RoundedCornerShape(10.dp),
+            border = BorderStroke(1.dp, Blue40),
+            onClick = {},
+            content = {
+                Text(text = stringResource(R.string.title_forgot_password).uppercase())
             }
         )
     }
